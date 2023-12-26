2cbf434c5e02201debed684232f670f84c8fb625
DebadritaGhosh <debadrita.ghosh2010@gmail.com>
2023-12-26 20:05:39 +0530

    added user schema

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,21 @@
-export const registerController = () => {
-    console.log("Register");
+// Importing services
+import { registerService } from "../services/auth.service.js";
+
+export const registerController = async (req,res,next) => {
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
 }
+
+export const loginController = async () => {
+    console.log("Login Controller");
+}
+
+export const forgetPasswordController = async () => {
+    console.log("Forget Password Controller");
+}
+
